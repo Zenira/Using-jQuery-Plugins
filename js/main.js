48f44f6dad10f60215a72f7af60fe1eb152f11ae
Lindsay Sauer <lindsay.sauer@gmail.com>
2016-05-17 12:11:42 -0400

    Updated files to add animsition

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(".animsition").animsition({
+		inClass: 'fade-in-right-lg',
+		outClass: 'fade-in-left-lg',
+		linkElement: 'header a',
+		inDuration: 1000,
+		outDuration: 500
+	});
+});
